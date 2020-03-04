5fe71dfa7af600b45485ec286333ab0182f58a71
Ivan <ivan.rodrigueztorres@santander.co.uk>
2020-03-04 09:15:55 +0000

    Initial commit

---
+++ src/test/java/soy/ivanrt/restExample/RestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package soy.ivanrt.restExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
